59db93fd91b6aeec9a69b90568cb80a36406a587
Archimedes Trajano <arch@trajano.net>
2012-07-17 07:52:20 -0400

    All projects have been converted

---
+++ net.trajano.openfolder.selectionprocessor.jdt/src/main/java/net/trajano/openfolder/selectionprocessor/jdt/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * These are the provided Selection Processors for the Open Folder Plug-In.
+ * It handles all the possible combinations with the Java Development environment of 
+ * Eclipse.  It is not designed to handle PDE, Team objects or non-standard views of Eclipse.
+ */
+package net.trajano.openfolder.selectionprocessor.jdt;
+
---
+++ net.trajano.openfolder.selectionprocessor/src/main/java/net/trajano/openfolder/selectionprocessor/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * These are the provided Selection Processors for the Open Folder
+ * Plug-In. It only handles things that are part of the core Eclipse 
+ * nothing JDT specific is provided.
+ */
+package net.trajano.openfolder.selectionprocessor;
+
